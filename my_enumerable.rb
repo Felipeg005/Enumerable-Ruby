8a9267430cae423254aa24403233ae7c24e38761
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-11-12 23:46:08 +0500

    add method for filter

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,19 +1,31 @@
+# frozen_string_literal: true
+
 module MyEnumerable
   def all?
     my_each do |i|
-    return true unless block_given?
-    true_false = yield(i)
-    return false unless true_false
+      return true unless block_given?
+
+      true_false = yield(i)
+      return false unless true_false
     end
-  return true
+    true
   end
+
   def any?
     my_each do |i|
       return false unless block_given?
+
       true_false = yield i
       return true unless true_false == false
     end
-    return false
+    false
+  end
+
+  def filter
+    result = []
+    my_each do |i|
+      result.push(i) if yield(i)
+    end
+    print result
   end
 end
-
--- my_list.rb
+++ my_list.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
+
 require_relative 'my_enumerable'
 
 class MyList
   include MyEnumerable
   def initialize(*list)
-    @list= list
+    @list = list
   end
-  def my_each
-    @list.each do |i|
-      yield i
-    end
+
+  def my_each(&block)
+    @list.each(&block)
   end
 end
 
 list = MyList.new(1, 2, 3, 4)
-puts(list.all? {|e| e < 5})
-puts(list.all? {|e| e > 5})
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
-puts(list.any? {|e| e == 2})
-puts(list.any? {|e| e == 5})
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
-
+puts(list.filter {|e| e.even?})
