26f3d27dcc0df722890970cda58af38c6e1eb7b0
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-11-12 23:54:19 +0500

    Add linters and update Readme.md

--- my_list.rb
+++ my_list.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'my_enumerable'
 
 class MyList
@@ -20,4 +18,4 @@ puts(list.all? { |e| e > 5 })
 puts(list.any? { |e| e == 2 })
 puts(list.any? { |e| e == 5 })
 
-puts(list.filter {|e| e.even?})
+puts(list.filter { |e| e.even? })
